3985b1325a726e483c4dbf8c4bbfdb7460346ffa
aleem-ahmed <w3st.io2021@gmail.com>
2021-12-19 19:33:56 -0500

    Improved

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
+// This is the script that deploys the smart contracts to the ethereum blockchain
 const TokenFarm = artifacts.require('TokenFarm')
+const TokenFarmm = artifacts.require('TokenFarmm')
 
 module.exports = function(deployer) {
+	console.log('deployed')
+
 	deployer.deploy(TokenFarm)
+
+	deployer.deploy(TokenFarmm)
 }
