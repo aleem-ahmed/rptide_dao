4953d32ffff9bf8fef74ca28cfc1d14b61ef171d
aleem-ahmed <opall.io2020@gmail.com>
2022-01-11 16:29:27 -0500

    Improved

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+// [IMPORT] //
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+// [IMPORT] Personal (Ordered by path) //
+
+
+// [INIT] //
+const routes = []
+
+
+Vue.use(VueRouter)
+
+
+const router = new VueRouter({
+	mode: 'history',
+	base: process.env.BASE_URL,
+	routes: routes,
+
+	// [VUE-ROUTER] Scroll Behavior //
+	scrollBehavior () { return { x: 0, y: 0 } }
+})
+
+// [VUE-ROUTER-SET-TITLE] //
+router.beforeEach((to, from, next) => {
+	document.title = to.meta.title + ' — ' + companyInfo.name
+	next()
+})
+
+export default router
+
+export { routes }
---
+++ src/store/index.js
@@ -0,0 +1,52 @@
+// [IMPORT] //
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+// [USE] //
+Vue.use(Vuex)
+
+
+export default new Vuex.Store({
+	state: {
+		node_env: localStorage.node_env == 'development' ? 'development' : 'production',
+
+		showIntro: true,
+		loading: false,
+		
+		showMenu: false,
+
+		userLogged: false,
+		user_decoded: {},
+
+		adminLogged: false,
+		admin_decoded: {},
+
+		isHomePage: false,
+
+		window: {
+			innerWidth: window.innerWidth
+		},
+
+		app: {
+			location: {
+				latitude: '',
+				longitude: '',
+			}
+		},
+	},
+
+	// Syncrous //
+	mutations: {
+		isLoading(state) { state.loading = true },
+
+		isNotLoading(state) { state.loading = false },
+	},
+
+	// Asyncronous //
+	actions: {},
+
+	getters: {},
+	
+	modules: {},
+});
